44eda229278290b24122e52231de4b5c36d85be8
cuijiufeng <1310639453@qq.com>
2020-06-09 19:22:20 +0800

    自己-day1

--- src/cn/algo/demo/AlgoDay1.java
+++ src/cn/algo/demo/AlgoDay1.java
@@ -1,5 +1,7 @@
 package cn.algo.demo;
 
+import java.util.Scanner;
+
 /**
  * @ClassName: AlgoDay1
  * @Description: day1
@@ -10,12 +12,38 @@ package cn.algo.demo;
  * 示例 1:
  * 输入: 12258
  * 输出: 5
- * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+ * 解释: 12258有5种不同的翻译，分别是'bccfi', 'bwfi', 'bczi', 'mcfi'和'mzi'
  * 提示：
  * 0 <= num < 231
  */
 public class AlgoDay1 {
+    private static final char str[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private static int cnt = 0;
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
+        f(String.valueOf(num), "");
+        System.out.println(cnt);
+    }
+
+    public static void f(String numStr, String rsStr){
+        if(numStr.isEmpty()){
+            System.out.println(rsStr);
+            cnt++;
+            return;
+        }
+        for(int i=1; i<=2; i++){
+            if(i==2 && (numStr.length()<2 || Integer.parseInt(numStr.substring(0, 2))>25)){
+                return;
+            }
+            Character t = null;
+            if(i == 1){
+                t =  str[numStr.charAt(0)-'0'];
+            } else if(i == 2){
+                t = str[Integer.parseInt((numStr.charAt(0)+""+numStr.charAt(1)))];
+            }
+            f(numStr.substring(i), rsStr+t);
+        }
     }
 }
