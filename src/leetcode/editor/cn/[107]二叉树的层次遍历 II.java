8bcefedca30bba9ed851d6296f689437fd72b5cb
cui <1310639453@qq.com>
2020-07-09 15:57:04 +0800

    第17天-中等

--- src/leetcode/editor/cn/[107]二叉树的层次遍历 II.java
+++ src/leetcode/editor/cn/[107]二叉树的层次遍历 II.java
@@ -1,10 +1,8 @@
 package leetcode.editor.cn;
-//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+//给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
 //
-// 
-//
-// 示例： 
-//二叉树：[3,9,20,null,null,15,7], 
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
 //
 //     3
 //   / \
@@ -13,23 +11,23 @@ package leetcode.editor.cn;
 //   15   7
 // 
 //
-// 返回其层次遍历结果： 
+// 返回其自底向上的层次遍历为： 
 //
 // [
-//  [3],
+//  [15,7],
 //  [9,20],
-//  [15,7]
+//  [3]
 //]
 // 
 // Related Topics 树 广度优先搜索
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Collectors;
-
 
 //Definition for a binary tree node.
 class TreeNode {
@@ -39,26 +37,27 @@ class TreeNode {
     TreeNode(int x) { val = x; }
 }
 
-//leetcode submit region begin(Prohibit modification and deletion)s
+//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public static void main(String[] args){
         Solution solu = new Solution();
-        TreeNode t1 = new TreeNode(1);
-        TreeNode t2 = new TreeNode(2);
-        TreeNode t3 = new TreeNode(3);
-        TreeNode t4 = new TreeNode(4);
-        TreeNode t5 = new TreeNode(5);
+        TreeNode t1 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(9);
+        TreeNode t3 = new TreeNode(20);
+        TreeNode t4 = new TreeNode(15);
+        TreeNode t5 = new TreeNode(7);
         t1.left = t2;
-        t2.left = t3;
+        t1.right = t3;
         t3.left = t4;
-        t4.left = t5;
+        t3.right = t5;
 
-        for (List<Integer> integers : solu.levelOrder(t1)) {
-            System.out.println(integers.toString());
+        List<List<Integer>> lists = solu.levelOrderBottom(t1);
+        for (List<Integer> list : lists) {
+            System.out.println(list.toString());
         }
     }
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> list = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedBlockingQueue<>();
         if (root == null){
@@ -67,19 +66,16 @@ class Solution {
 
         queue.add(root);
         while (!queue.isEmpty()){
-            //队列的大小就是一层中结点的个数
             int size = queue.size();
             List<TreeNode> t = new ArrayList<>();
-            //把一层中的结点都取出来
             for (int i = 0; i < size; i++) {
                 t.add(queue.poll());
             }
             List<Integer> collect = t.stream().map(treeNode -> treeNode.val).collect(Collectors.toList());
             list.add(collect);
 
-            //遍历这一层中的结点，如果有左右子树，全部进队
             for (TreeNode treeNode : t) {
-                if (treeNode.left != null) {
+                if (treeNode.left != null){
                     queue.add(treeNode.left);
                 }
                 if (treeNode.right != null){
@@ -87,6 +83,7 @@ class Solution {
                 }
             }
         }
+        Collections.reverse(list);
         return list;
     }
 }
