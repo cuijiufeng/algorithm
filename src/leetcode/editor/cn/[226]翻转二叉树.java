188ab8077dccb59ba1747c4947a73a4dadee8b50
cui <1310639453@qq.com>
2020-07-14 14:03:05 +0800

    第20天-中等

---
+++ src/leetcode/editor/cn/[226]翻转二叉树.java
@@ -0,0 +1,57 @@
+package leetcode.editor.cn;
+//翻转一棵二叉树。
+//
+// 示例： 
+//
+// 输入： 
+//
+//      4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9 
+//
+// 输出： 
+//
+//      4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1 
+//
+// 备注: 
+//这个问题是受到 Max Howell 的 原问题 启发的 ： 
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。 
+// Related Topics 树
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)){
+            return root;
+        }
+        f(root);
+        return root;
+    }
+
+    public void f(TreeNode root){
+        if (root.left == null && root.right == null){
+            return;
+        }
+        TreeNode t = root.left;
+        root.left = root.right;
+        root.right = t;
+        invertTree(root.left);
+        invertTree(root.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
