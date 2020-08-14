81dd6f7107770e8d49cc7a9ad70ac2b7c68b2b3c
cui <1310639453@qq.com>
2020-08-14 16:02:54 +0800

    第28天-简单

---
+++ src/leetcode/editor/cn/[693]交替位二进制数.java
@@ -0,0 +1,61 @@
+//给定一个正整数，检查他是否为交替位二进制数：换句话说，就是他的二进制数相邻的两个位数永不相等。
+//
+// 示例 1: 
+//
+// 
+//输入: 5
+//输出: True
+//解释:
+//5的二进制数是: 101
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: 7
+//输出: False
+//解释:
+//7的二进制数是: 111
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: 11
+//输出: False
+//解释:
+//11的二进制数是: 1011
+// 
+//
+// 示例 4: 
+//
+// 
+//输入: 10
+//输出: True
+//解释:
+//10的二进制数是: 1010
+// 
+// Related Topics 位运算 
+// 👍 74 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public static void main(String[] args){
+        Solution solu = new Solution();
+        System.out.println(solu.hasAlternatingBits(5));
+    }
+    public boolean hasAlternatingBits(int n) {
+        int t = n & 1;
+        n >>= 1;
+        while (n != 0){
+            if ((n & 1) == t) {
+                return false;
+            }
+            t = n & 1;
+            n >>= 1;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
