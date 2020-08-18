8c8746653dbb25196def1735b17c22f7b249435c
cui <1310639453@qq.com>
2020-08-18 15:58:36 +0800

    第30天-中等

---
+++ src/leetcode/editor/cn/[678]有效的括号字符串.java
@@ -0,0 +1,68 @@
+//给定一个只包含三种字符的字符串：（ ，） 和 *，写一个函数来检验这个字符串是否为有效字符串。有效字符串具有如下规则： 
+//
+// 
+// 任何左括号 ( 必须有相应的右括号 )。 
+// 任何右括号 ) 必须有相应的左括号 ( 。 
+// 左括号 ( 必须在对应的右括号之前 )。 
+// * 可以被视为单个右括号 ) ，或单个左括号 ( ，或一个空字符串。 
+// 一个空字符串也被视为有效字符串。 
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: "()"
+//输出: True
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: "(*)"
+//输出: True
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: "(*))"
+//输出: True
+// 
+//
+// 注意: 
+//
+// 
+// 字符串大小将在 [1，100] 范围内。 
+// 
+// Related Topics 字符串 
+// 👍 144 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public static void main(String[] args){
+        Solution solu = new Solution();
+        System.out.println(solu.checkValidString("(*))"));
+    }
+    public boolean checkValidString(String s) {
+        return f(s.toCharArray(), 0, 0);
+    }
+
+    public boolean f(char[] arr, int pos, int balance){
+        if (balance<0){
+            return false;
+        }
+        if (pos == arr.length){
+            return balance == 0;
+        }
+        if (arr[pos] == '('){
+            return f(arr, pos+1, balance+1);
+        } else if (arr[pos] == ')'){
+            return f(arr, pos+1, balance-1);
+        } else if (arr[pos] == '*') {
+            return f(arr, pos+1, balance+1) || f(arr, pos+1, balance-1) || f(arr, pos+1, balance);
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
