c0341d10fb5b449d413a52df39a6312023916218
cuijiufeng <cui-jiufeng@qq.com>
2021-09-30 16:50:01 +0800

    2021.9.30-简单

---
+++ src/leetcode/editor/cn/[628]三个数的最大乘积.java
@@ -0,0 +1,66 @@
+//给你一个整型数组 nums ，在数组中找出由三个数组成的最大乘积，并输出这个乘积。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：6
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,3,4]
+//输出：24
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [-1,-2,-3]
+//输出：-6
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 3 <= nums.length <= 104 
+// -1000 <= nums[i] <= 1000 
+// 
+// Related Topics 数组 数学 排序 
+// 👍 322 👎 0
+
+
+package leetcode.editor.cn;
+
+
+class MaximumProductOfThreeNumbers {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximumProduct(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int t = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = t;
+                }
+            }
+        }
+        int i = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        int i1 = nums[nums.length - 1] * nums[0] * nums[1];
+        return Math.max(i, i1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public static void main(String[] args) {
+        Solution solution = new MaximumProductOfThreeNumbers().new Solution();
+        //918771264 期望结果:972256230
+        System.out.println(solution.maximumProduct(new int[]{-710,-107,-851,657,-14,-859,278,-182,-749,718,-640,127,-930,-462,694,969,143,309,904,-651,160,451,-159,-316,844,-60,611,-169,-73,721,-902,338,-20,-890,-819,-644,107,404,150,-219,459,-324,-385,-118,-307,993,202,-147,62,-94,-976,-329,689,870,532,-686,371,-850,-186,87,878,989,-822,-350,-948,-412,161,-88,-509,836,-207,-60,771,516,-287,-366,-512,509,904,-459,683,-563,-766,-837,-333,93,893,303,908,532,-206,990,280,826,-13,115,-732,525,-939,-787}));
+    }
+}
